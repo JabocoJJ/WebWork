f81f23c9ff34787623af269b63ca76d0b006eae4
Jacob <spikey.1999@live.co.uk>
2020-11-27 01:19:47 +0000

    WebWork

---
+++ Pages/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebWork.Pages
+{
+    public class DBConnection
+    {
+        public string DbString()
+        {
+            string dbString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DatabaseWeb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            return dbString;        
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function toggle(source) {
+    var checkBoxes = document.querySelectorAll("input[name^='EmployeeRecord.EmployeeTraining[']");
+
+    for (var i = 0; i < checkBoxes.length; i++) {
+        if (checkBoxes[i].type == "checkbox")
+            checkBoxes[i].checked = source.checked;
+    }
+}
+function hideList() {
+    var x = document.getElementById("navList");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+};
